79530ee94e6072f96efbdb020d84f9de25a11d85
Harry Lachenmayer <harrylachenmayer@gmail.com>
2012-02-25 20:35:09 +0000

    Hooked up top shops

--- js/script.js
+++ js/script.js
@@ -2,24 +2,24 @@
 
  */
 
-var Foodie = (function () {
+var Foodie = (function() {
 
-    var getPurchasesByCID = function (cid) {
-        $.get("http://129.31.217.212/hthon/main/?type=cid&id=" + cid + "&latest=30",
-            function (response) {
-                console.log(response);
-                return JSON.parse(response);
-            });
-    }
+  var userInfo = {};
 
-    return {
-        testPurchases: function () {
-            return getPurchasesByCID(691254);
-        }
+  return {
+    getUserInfoByCID: function(cid, callback) {
+      $.get("http://dyn1212-5.wlan.ic.ac.uk/~peregrinepark/ImperialHackathon/dbase.php?cid=" + cid, function(response) {
+        userInfo[cid] = JSON.parse(response);
+        callback(userInfo[cid]);
+      });
+      //var fakeResponse = '{"dailybreakdown":[],"topfoods":[{"itemid":"1","quantity":"1","total":"1200"},{"itemid":"8","quantity":"1","total":"50"}],"topshops":[{"shopid":"4","count":"1","total":"1200"},{"shopid":"1","count":"1","total":"50"}],"items":[{"itemid":"1","name":"Tuna Sandwich","price":"1200","metadata":"1,5,400,1,3"},{"itemid":"8","name":"Apple","price":"50","metadata":"1,2,3,200,2,2"}],"shops":[{"shopid":"1","name":"JCR Pancake Shop"},{"shopid":"4","name":"JCR Fast Food"}]}';
+      //callback(JSON.parse(fakeResponse));
     }
+  }
+
 })();
 
-$(function () {
+var DailyBreakdown = (function() {
 
     var dailyBreakdownData = [
         [0, 2.34],
@@ -29,29 +29,11 @@ $(function () {
         [4, 2.54]
     ];
 
-    var dailyBreakdownDays = [
-        [
-            [0, 2.34]
-        ],
-        [
-            [1, 5.14]
-        ],
-        [
-            [2, 3.31]
-        ],
-        [
-            [3, 6.24]
-        ],
-        [
-            [4, 2.54]
-        ]
-    ];
-
-    var seperateDays = new Array();
+    var separateDays = [];
     var dayColors = ["#000000", "#F24949", "#89E0FA", "#FFEC73", "#73C75A"];
-    for(var i = 0; i < dailyBreakdownDays.length; i++){
+    for(var i = 0; i < dailyBreakdownData.length; i++){
         var series = {
-            data: dailyBreakdownDays[i],
+            data: [dailyBreakdownData[i]],
             lines: {
                 show: false
             },
@@ -63,7 +45,7 @@ $(function () {
                 lineWidth: 4
             }
         }
-        seperateDays.push(series);
+        separateDays.push(series);
     }
 
     var dailyBreakdownSeries = {
@@ -73,7 +55,7 @@ $(function () {
             lineWidth: 5
         },
         shadowSize: 0,
-        color: "rgb(160, 160, 160)",
+        color: "rgb(180, 180, 180)",
         clickable: true,
         hoverable: true,
         points: {
@@ -82,7 +64,7 @@ $(function () {
         }
     }
 
-    seperateDays.unshift(dailyBreakdownSeries);
+    separateDays.unshift(dailyBreakdownSeries);
 
     var options = {
         legend: {
@@ -98,13 +80,64 @@ $(function () {
             borderWidth: 0
         }
     };
+  
+  return {
+    plotGraph: function() {
+      $.plot($("#graph"), separateDays, options);
+    }
+  }
 
-    console.log(seperateDays);
+})();
 
-    $.plot($("#graph"), seperateDays, options);
+var TopShops = (function() {
 
-    $("graph").bind("plothover", function (event, pos, item) {
-        console.log([event, pos, item]);
+  var topShops = {};
+  var shops = {};
+  
+  var getShopNameById = function(id) {
+    var shopName = "";
+    $.each(shops, function(i, shop) {
+      console.log("id: " + id + " shopid: " + shop.shopid);
+      if (id == shop.shopid) {
+        console.log("return wooo: " + shop.name);
+        shopName = shop.name;
+        return;
+      }
     });
+    return shopName;
+  }
 
+  return {
+    
+    getInfo: function(info) {
+      topShops = info.topshops;
+      shops = info.shops;
+    },
+
+    drawTable: function() {
+      $.each(topShops, function(i, shop) {
+        console.log(getShopNameById(shop.shopid));
+        var tableRow = "<tr><td class=shop>" +
+                       getShopNameById(shop.shopid) +
+                       "</td> <td class=amount>" +
+                       shop.total +
+                       "</td></tr>";
+        $("#topshops").append(tableRow);
+      });
+    }
+
+  }
+
+})();
+
+$(function () {
+  
+  Foodie.getUserInfoByCID(593824, function(userInfo) {
+    TopShops.getInfo(userInfo);
+    TopShops.drawTable();
+  });
+  
+
+  DailyBreakdown.plotGraph();
+  
 });
