898d9f96d442f885e4ed541727d953f3c0e36367
Harry Lachenmayer <harrylachenmayer@gmail.com>
2012-02-25 17:22:29 +0000

    Add first graph

--- js/script.js
+++ js/script.js
@@ -2,25 +2,49 @@
 
 */
 
+$(function() {
+  
+  var dailyBreakdownData = [[0,2.34],
+                            [1,5.14],
+                            [2,3.31],
+                            [3,6.24],
+                            [4,2.54]];
 
+  var dailyBreakdownSeries = {
+      data: dailyBreakdownData,
+      lines: {
+        show: true,
+        lineWidth: 5
+      },
+      shadowSize: 0,
+      color: "rgb(160, 160, 160)",
+      clickable: true,
+      hoverable: true,
+      points: {
+        show: true,
+        radius: 5 
+      }
+  }
 
+  var options = {
+    legend: {
+      show: false
+    },
+    xaxis: {
+      show: false
+    },
+    yaxis: {
+      show: false
+    },
+    grid: {
+      borderWidth: 0
+    }
+  };
 
+  $.plot($("#graph"), [dailyBreakdownSeries], options);
 
+  $("graph").bind("plothover", function(event, pos, item) {
+    console.log([event, pos, item]);
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
